908c239c30442123e5cb32cc4ab6e5cc87c82092
bigsnarfdude <ohprecio@gmail.com>
2012-11-18 00:06:51 -0500

    class inheritance

---
+++ animals.py
@@ -0,0 +1,26 @@
+class Animal:
+    def make_noise(self):
+        print "animal"
+
+class Dog(Animal):
+    def make_noise(self):
+        print "woof"
+
+class Cat(Animal):
+    def make_noise(self):
+        print "meow"
+
+class Shark(Animal):
+    def __new__
+
+    def _noise(self):
+        print "blub"
+
+    def foo()
+    
+animals = [ Cat(), Shark(), Dog(), Animal(), Dog() ]
+
+for animal in animals:
+    animal.make_noise()
+
+

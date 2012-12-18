58d4259a7e3e9e4aac1ed7c1ed5aa088dcda3200
bigsnarfdude <ohprecio@gmail.com>
2012-12-17 18:44:13 -0800

    functional try2

--- fizzbuzzFunctional.py
+++ fizzbuzzFunctional.py
@@ -1,11 +1,11 @@
 alist = range(1,101)
 
 def fizz(number):
-    if number%3 == 0:
+    if number % 3 == 0:
         return "Fizz"
 
 def buzz(number):
-    if number%5 == 0:
+    if number % 5 == 0:
         return "Buzz"
 
 def fizzbuzz(number):
@@ -15,7 +15,7 @@ def fizzbuzz(number):
 def number(number):
     return number
 
-[ fizzbuzz(item) or fizz(item) or buzz(item) or number(item) for item in alist ]
+print [ fizzbuzz(item) or fizz(item) or buzz(item) or number(item) for item in alist ]
 
 
 
